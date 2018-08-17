a53807c8c0037f4a52ff982c86e6608ea3ff9201
jhon <garcijhon24@gmail.com>
2018-08-17 10:56:27 -0500

    relacionesf

--- app/User.php
+++ app/User.php
@@ -33,4 +33,9 @@ class User extends Authenticatable
       return $this->hasMany('App\Areas');
   }
 
+  public function areauserUser()
+  {
+      return $this->hasMany('App\area_users');
+  }
+
 }
